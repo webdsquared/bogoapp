081bf21173b587c700badd1fb88865d3aef661c2
Aaron Johnson <aaronjohnsonis@gmail.com>
2013-06-11 14:06:15 -0500

    initial commit

---
+++ bogoapp/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bogoapp::Application.config.secret_token = '04749f99ad07b03b9bcc80448dc80f10620112baaa6bb35f62a1e128bf80c893521644be608aa3d840e6ef0774a7b778531ebe3481bd753a2072477bf4aa9398'
